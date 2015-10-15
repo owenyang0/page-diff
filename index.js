7dc5af664f31bbc100274cf8217264c00bd489ef
Owen Yang <usay@outlook.com>
2015-10-16 00:01:34 +0800

    add some basic format funcs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('babel/register')
 var app = require('./src')
-
 var output = require('./src/output')
 
+var urlA = 'https://www.npmjs.com/search?q=table'
+var urlB = 'https://www.npmjs.com/search?q=cli-table'
+
 app
-.diff('https://www.baidu.com/', 'https://www.baidu.com/')
-.then(function (patches) {
-  console.log(output(patches))
-})
-.catch(console.error)
+  .diff(urlA, urlB)
+  .then(function (patches) {
+    console.log(output(patches))
+  })
+  .catch(console.error)
--- src/output.js
+++ src/output.js
@@ -1,20 +1,71 @@
 import Table from 'cli-table'
 
+// VirtualPatch.NONE = 0
+// VirtualPatch.VTEXT = 1
+// VirtualPatch.VNODE = 2
+// VirtualPatch.WIDGET = 3
+// VirtualPatch.PROPS = 4
+// VirtualPatch.ORDER = 5
+// VirtualPatch.INSERT = 6
+// VirtualPatch.REMOVE = 7
+// VirtualPatch.THUNK = 8
+
 var table = new Table({
-  head: ['id', 'tag', 'properties', 'value']
+  head: ['id', 'first value', 'second value', 'tag', 'properties'],
+  colWidths: [5, 50, 50, 20, 30]
 })
 
 function formatPatches (patches) {
   return patches.map(function (pc, idx) {
-    var props = pc.vNode.properties
+    return [idx].concat(format(pc))
+  })
+}
+
+function format (patch) {
+  var typeFuns = {
+    '1': textPatch,
+    '4': propPatch
+  }
+
+  return selectFormat(typeFuns, patch.type)(patch)
+}
+
+function selectFormat (mappingFuns, type) {
+  return mappingFuns[type] || defaultPatch
+}
+
+function propPatch (patch) {
+  var tagFuns = {
+    'INPUT': inputPatch
+  }
+
+  var tag = patch.vNode.tagName
+  var tagFun = selectFormat(tagFuns, tag)
+
+  return tagFun(patch)
+
+  function inputPatch (patch) {
+    var props = patch.vNode.properties
+    var outProps = ['#', props.id || 'null', ' .', props.className || 'null'].join('')
 
     return [
-      idx,
-      pc.vNode.tagName,
-      ['#', props.id, ' .', props.className].join(''),
-      pc.patch.value.value
+      props.value.value,
+      patch.patch.value.value,
+      tag,
+      outProps
     ]
-  })
+  }
+}
+
+function textPatch (patch) {
+  return [patch.vNode.text, patch.patch.text]
+}
+
+function defaultPatch (patch) {
+  var type = patch.type
+  var tag = patch.vNode && patch.vNode.tagName
+
+  return ['Haven\'t been implemented', '', 'type ' + type + ' | ' + tag]
 }
 
 export default function (patches) {
@@ -22,4 +73,5 @@ export default function (patches) {
 
   return table.toString()
 }
+
 
